eab3d717209f4cd71102bc579aadabde06a6c910
HetBorda0 <hetborda.2005@gmail.com>
2025-06-16 13:18:38 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Integration Ninjas
+       Het Borda - 22DIT006
         </p>
         <a
           className="App-link"
